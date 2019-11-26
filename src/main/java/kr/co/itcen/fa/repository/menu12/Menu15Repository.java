85c18fc2413bab7d42c118d342e2185869de5b26
bit <bit@DESKTOP-S8HQR48>
2019-11-26 15:17:25 +0900

    menu12 commmit

---
+++ src/main/java/kr/co/itcen/fa/repository/menu12/Menu15Repository.java
@@ -0,0 +1,28 @@
+package kr.co.itcen.fa.repository.menu12;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kr.co.itcen.fa.vo.menu12.TestVo;
+
+/**
+ * 
+ * @author 양홍석
+ * 매출거래처관리
+ *
+ */
+@Repository
+public class Menu15Repository {
+
+	@Autowired
+	private SqlSession sqlSession;
+
+	public void test() {
+		TestVo testVo = new TestVo();
+		testVo.setName("양홍석");
+		sqlSession.insert("menu15.save", testVo);
+	}
+	
+	
+}
