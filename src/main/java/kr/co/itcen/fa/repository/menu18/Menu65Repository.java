7a7f5ee85bad63aa2f36b7b9ce51e1062d52b408
Tae Y. Ahn <forcetyty@gmail.com>
2019-11-28 21:31:01 +0900

    관리자 : 공통코드 화면 구현 : 공통코드 추가 기능 구현 : List 미 구현

--- src/main/java/kr/co/itcen/fa/repository/menu18/Menu65Repository.java
+++ src/main/java/kr/co/itcen/fa/repository/menu18/Menu65Repository.java
@@ -1,9 +1,12 @@
 package kr.co.itcen.fa.repository.menu18;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import kr.co.itcen.fa.vo.SectionVo;
 import kr.co.itcen.fa.vo.menu18.TestVo;
 
 /**
@@ -21,4 +24,20 @@ public class Menu65Repository {
 		testVo.setName("안대혁");
 		sqlSession.insert("menu65.save", testVo);
 	}
+	
+	// 품목코드 저장 메소드
+	public void save(SectionVo sectionVo) {
+		sqlSession.insert("section.save", sectionVo);
+	}
+	
+	public List<SectionVo> findAllByParentNoNull(){
+		return sqlSession.selectList("section.findAllByParentNoNull");
+	}
+	
+	// 대분류명을 SelectBox에 표시해주는 메소드
+	public List<SectionVo> findAll(){
+		return sqlSession.selectList(null);
+	}
+	
+	
 }
--- src/main/java/kr/co/itcen/fa/service/menu18/Menu65Service.java
+++ src/main/java/kr/co/itcen/fa/service/menu18/Menu65Service.java
@@ -1,9 +1,14 @@
 package kr.co.itcen.fa.service.menu18;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.co.itcen.fa.dto.Section;
 import kr.co.itcen.fa.repository.menu18.Menu65Repository;
+import kr.co.itcen.fa.vo.SectionVo;
 
 /**
  * 
@@ -19,4 +24,27 @@ public class Menu65Service {
 	public void test() {
 		menu65Repository.test();
 	}
+	
+	public List<SectionVo> getAllMainSection(){
+		return menu65Repository.findAllByParentNoNull();
+	}
+	
+	public void addSection(SectionVo sectionVo) {
+		menu65Repository.save(sectionVo);
+		
+	}
+	
+	// 모든 리스트를 출력해주는 메소드
+	public List<Section> getAllSection(){
+		List<Section> result = new ArrayList<>();
+		
+		Section listSection = null;
+		List<SectionVo> list = menu65Repository.findAll();
+		
+		return result;
+	}
+	
+	
+	
+	 
 }
