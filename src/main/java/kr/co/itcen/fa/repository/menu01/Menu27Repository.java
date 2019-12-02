7a00bb83ffdca7c854137b868a16b76474e4ab2b
이종윤님 <leejy3653@naver.com>
2019-12-02 21:07:23 +0900

    1팀:거래처관리 주소api적용 완료, CRUD #1

--- src/main/java/kr/co/itcen/fa/repository/menu01/Menu27Repository.java
+++ src/main/java/kr/co/itcen/fa/repository/menu01/Menu27Repository.java
@@ -1,9 +1,12 @@
 package kr.co.itcen.fa.repository.menu01;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import kr.co.itcen.fa.vo.menu01.CustomerVo;
 import kr.co.itcen.fa.vo.menu11.TestVo;
 
 /**
@@ -22,6 +25,25 @@ public class Menu27Repository {
 		TestVo testVo = new TestVo();
 		testVo.setName("거래처관리");
 		sqlSession.insert("menu27.save", testVo);
+	}
+
+	public Boolean insert(CustomerVo customerVo) {
+		int count = sqlSession.insert("menu27.insert", customerVo);
+		return count==1;
+			
+	}
+
+	public void update(CustomerVo customerVo) {
+		sqlSession.update("menu27.update", customerVo);
+	}
+
+	public void delete(CustomerVo customerVo) {
+		sqlSession.delete("menu27.delete", customerVo);
+		
+	}
+
+	public List<CustomerVo> list() {
+		return sqlSession.selectList("menu27.getList");
 	}
 
 }
--- src/main/java/kr/co/itcen/fa/service/menu01/Menu27Service.java
+++ src/main/java/kr/co/itcen/fa/service/menu01/Menu27Service.java
@@ -1,9 +1,12 @@
 package kr.co.itcen.fa.service.menu01;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.itcen.fa.repository.menu01.Menu27Repository;
+import kr.co.itcen.fa.vo.menu01.CustomerVo;
 
 /**
  * 
@@ -14,11 +17,30 @@ import kr.co.itcen.fa.repository.menu01.Menu27Repository;
 
 @Service
 public class Menu27Service {
+	
 	@Autowired
 	private Menu27Repository menu27Repository;
 
 	public void test() {
 		menu27Repository.test();
+	}
+	
+	public void insert(CustomerVo customerVo) {
+		menu27Repository.insert(customerVo);
+	}
+
+	public void update(CustomerVo customerVo) {
+		menu27Repository.update(customerVo);
+	}
+
+	public void delete(CustomerVo customerVo) {
+		menu27Repository.delete(customerVo);
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<CustomerVo> list() {
+		return menu27Repository.list();
 	}
 
 }
