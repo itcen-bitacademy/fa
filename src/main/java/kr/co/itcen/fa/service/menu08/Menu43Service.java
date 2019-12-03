66401d7b7df092071852974f23474e1aa257075f
Xnote <Xnote@LG>
2019-12-03 16:25:22 +0900

    무형자산관리 CRUD

--- src/main/java/kr/co/itcen/fa/service/menu08/Menu43Service.java
+++ src/main/java/kr/co/itcen/fa/service/menu08/Menu43Service.java
@@ -1,9 +1,12 @@
 package kr.co.itcen.fa.service.menu08;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.itcen.fa.repository.menu08.Menu43Repository;
+import kr.co.itcen.fa.vo.menu08.IntangibleAssetsVo;
 
 
 /**
@@ -20,5 +23,21 @@ public class Menu43Service {
 	
 	public void test() {
 		menu43Repository.test();
+	}
+	
+	public void insert(IntangibleAssetsVo intangibleAssetsVo) {
+		menu43Repository.insert(intangibleAssetsVo);
+	}
+
+	public List<IntangibleAssetsVo> getList() {
+		return menu43Repository.getList();
+	}
+
+	public void update(IntangibleAssetsVo intangibleAssetsVo) {
+		menu43Repository.update(intangibleAssetsVo);
+	}
+
+	public void delete(String id) {
+		menu43Repository.delete(id);
 	}
 }
