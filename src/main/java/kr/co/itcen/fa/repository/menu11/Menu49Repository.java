b5f489d38f1aa622c88db1cc9a9d5dfe746db670
kimjunho <widn45@naver.com>
2019-12-03 20:49:49 +0900

    #4팀 list 구현

--- src/main/java/kr/co/itcen/fa/repository/menu11/Menu49Repository.java
+++ src/main/java/kr/co/itcen/fa/repository/menu11/Menu49Repository.java
@@ -1,9 +1,16 @@
 package kr.co.itcen.fa.repository.menu11;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import kr.co.itcen.fa.util.PaginationUtil;
+import kr.co.itcen.fa.vo.SectionVo;
+import kr.co.itcen.fa.vo.menu11.LTermdebtVo;
 import kr.co.itcen.fa.vo.menu11.TestVo;
 
 /**
@@ -22,6 +29,23 @@ public class Menu49Repository {
 		TestVo testVo = new TestVo();
 		testVo.setName("김준호");
 		sqlSession.insert("menu49.save", testVo);
+	}
+
+	public int listCount(LTermdebtVo vo) {
+		return sqlSession.selectOne("menu49.selectAllCount",vo);
+	}
+
+	public List<LTermdebtVo> list(PaginationUtil pagination,LTermdebtVo vo) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("pagination", pagination);
+		map.put("vo", vo);
+		List<LTermdebtVo> list= sqlSession.selectList("menu49.selectAll",map);
+		return list;
+	}
+
+	public List<SectionVo> selectSection() {
+		
+		return sqlSession.selectList("menu49.selectsection");
 	}
 	
 }
--- src/main/java/kr/co/itcen/fa/service/menu11/Menu49Service.java
+++ src/main/java/kr/co/itcen/fa/service/menu11/Menu49Service.java
@@ -1,9 +1,15 @@
 package kr.co.itcen.fa.service.menu11;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.co.itcen.fa.dto.DataResult;
 import kr.co.itcen.fa.repository.menu11.Menu49Repository;
+import kr.co.itcen.fa.util.PaginationUtil;
+import kr.co.itcen.fa.vo.SectionVo;
+import kr.co.itcen.fa.vo.menu11.LTermdebtVo;
 
 /**
  * 
@@ -19,5 +25,23 @@ public class Menu49Service {
 	
 	public void test() {
 		menu49Repository.test();
+	}
+
+	public DataResult<LTermdebtVo> list(int page,LTermdebtVo vo) {
+DataResult<LTermdebtVo> dataResult = new DataResult<LTermdebtVo>();
+		
+		int totalCnt = menu49Repository.listCount(vo);
+		
+		PaginationUtil pagination = new PaginationUtil(page, totalCnt, 11, 5);
+		dataResult.setPagination(pagination);
+		List<LTermdebtVo> list = menu49Repository.list(pagination,vo);
+		dataResult.setDatas(list);
+		
+		return dataResult;
+	}
+
+	public List<SectionVo> selectSection() {
+		// TODO Auto-generated method stub
+		return menu49Repository.selectSection();
 	}
 }
