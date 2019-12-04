d2c2af8512952e04f0311fa2acabe2a358a17256
JeaGunix <dlwprn1541@gmail.com>
2019-12-04 21:32:49 +0900

    37,38

---
+++ src/main/java/kr/co/itcen/fa/vo/menu02/BuyTaxbillItemsVo.java
@@ -0,0 +1,94 @@
+package kr.co.itcen.fa.vo.menu02;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("buytaxbillitems")
+public class BuyTaxbillItemsVo {
+	private Long no;
+	private String taxbillNo;
+	private String purchaseDate;
+	private String itemName;
+	private Long amount;
+	private Long supplyValue;
+	private Long taxValue;
+	private String insertUserid;
+	private String insertDay;
+	private String updateUserid;
+	private String updateDay;
+	@Override
+	public String toString() {
+		return "BuyTaxbillItemsVo [no=" + no + ", taxbillNo=" + taxbillNo + ", purchaseDate=" + purchaseDate
+				+ ", itemName=" + itemName + ", amount=" + amount + ", supplyValue=" + supplyValue + ", taxValue="
+				+ taxValue + ", insertUserid=" + insertUserid + ", insertDay=" + insertDay + ", updateUserid="
+				+ updateUserid + ", updateDay=" + updateDay + "]";
+	}
+	public Long getNo() {
+		return no;
+	}
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	
+	public String getTaxbillNo() {
+		return taxbillNo;
+	}
+	public void setTaxbillNo(String taxbillNo) {
+		this.taxbillNo = taxbillNo;
+	}
+	public String getPurchaseDate() {
+		return purchaseDate;
+	}
+	public void setPurchaseDate(String purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public Long getAmount() {
+		return amount;
+	}
+	public void setAmount(Long amount) {
+		this.amount = amount;
+	}
+	public Long getSupplyValue() {
+		return supplyValue;
+	}
+	public void setSupplyValue(Long supplyValue) {
+		this.supplyValue = supplyValue;
+	}
+	public Long getTaxValue() {
+		return taxValue;
+	}
+	public void setTaxValue(Long taxValue) {
+		this.taxValue = taxValue;
+	}
+	public String getInsertUserid() {
+		return insertUserid;
+	}
+	public void setInsertUserid(String insertUserid) {
+		this.insertUserid = insertUserid;
+	}
+	public String getInsertDay() {
+		return insertDay;
+	}
+	public void setInsertDay(String insertDay) {
+		this.insertDay = insertDay;
+	}
+	public String getUpdateUserid() {
+		return updateUserid;
+	}
+	public void setUpdateUserid(String updateUserid) {
+		this.updateUserid = updateUserid;
+	}
+	public String getUpdateDay() {
+		return updateDay;
+	}
+	public void setUpdateDay(String updateDay) {
+		this.updateDay = updateDay;
+	}
+	
+
+}
--- src/main/java/kr/co/itcen/fa/vo/menu02/BuyTaxbillVo.java
+++ src/main/java/kr/co/itcen/fa/vo/menu02/BuyTaxbillVo.java
@@ -4,8 +4,9 @@ import org.apache.ibatis.type.Alias;
 
 @Alias("buytaxbillvo")
 public class BuyTaxbillVo {
-	private Long no;
+	private String no;
 	private String manageNo;
+	private String companyName;
 	private String writeDate;
 	private Long totalSupplyValue;
 	private Long totalTaxValue;
@@ -15,100 +16,85 @@ public class BuyTaxbillVo {
 	private String insertDay;
 	private String updateUserid;
 	private String updateDay;
-
-	public Long getNo() {
+	@Override
+	public String toString() {
+		return "BuyTaxbillVo [no=" + no + ", manageNo=" + manageNo + ", companyName=" + companyName + ", writeDate="
+				+ writeDate + ", totalSupplyValue=" + totalSupplyValue + ", totalTaxValue=" + totalTaxValue
+				+ ", taxType=" + taxType + ", deleteFlag=" + deleteFlag + ", insertUserid=" + insertUserid
+				+ ", insertDay=" + insertDay + ", updateUserid=" + updateUserid + ", updateDay=" + updateDay + "]";
+	}
+	public String getNo() {
 		return no;
 	}
-
-	public void setNo(Long no) {
+	public void setNo(String no) {
 		this.no = no;
 	}
-
 	public String getManageNo() {
 		return manageNo;
 	}
-
 	public void setManageNo(String manageNo) {
 		this.manageNo = manageNo;
 	}
-
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
 	public String getWriteDate() {
 		return writeDate;
 	}
-
 	public void setWriteDate(String writeDate) {
 		this.writeDate = writeDate;
 	}
-
 	public Long getTotalSupplyValue() {
 		return totalSupplyValue;
 	}
-
 	public void setTotalSupplyValue(Long totalSupplyValue) {
 		this.totalSupplyValue = totalSupplyValue;
 	}
-
 	public Long getTotalTaxValue() {
 		return totalTaxValue;
 	}
-
 	public void setTotalTaxValue(Long totalTaxValue) {
 		this.totalTaxValue = totalTaxValue;
 	}
-
 	public String getTaxType() {
 		return taxType;
 	}
-
 	public void setTaxType(String taxType) {
 		this.taxType = taxType;
 	}
-
 	public String getDeleteFlag() {
 		return deleteFlag;
 	}
-
 	public void setDeleteFlag(String deleteFlag) {
 		this.deleteFlag = deleteFlag;
 	}
-
 	public String getInsertUserid() {
 		return insertUserid;
 	}
-
 	public void setInsertUserid(String insertUserid) {
 		this.insertUserid = insertUserid;
 	}
-
 	public String getInsertDay() {
 		return insertDay;
 	}
-
 	public void setInsertDay(String insertDay) {
 		this.insertDay = insertDay;
 	}
-
 	public String getUpdateUserid() {
 		return updateUserid;
 	}
-
 	public void setUpdateUserid(String updateUserid) {
 		this.updateUserid = updateUserid;
 	}
-
 	public String getUpdateDay() {
 		return updateDay;
 	}
-
 	public void setUpdateDay(String updateDay) {
 		this.updateDay = updateDay;
 	}
-
-	@Override
-	public String toString() {
-		return "BuyTaxbillVo [no=" + no + ", manageNo=" + manageNo + ", writeDate=" + writeDate + ", totalSupplyValue="
-				+ totalSupplyValue + ", totalTaxValue=" + totalTaxValue + ", taxType=" + taxType + ", deleteFlag="
-				+ deleteFlag + ", insertUserid=" + insertUserid + ", insertDay=" + insertDay + ", updateUserid="
-				+ updateUserid + ", updateDay=" + updateDay + "]";
-	}
+	
+	
 }
