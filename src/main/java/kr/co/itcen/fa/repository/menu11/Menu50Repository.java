a4dcac5e2d7ebd9fd6e48db5ead4b164a4ff52b9
Junho Park <jhpark870708@daum.net>
2019-12-18 13:39:07 +0900

    4팀 사채관리 commit

--- src/main/java/kr/co/itcen/fa/repository/menu11/Menu50Repository.java
+++ src/main/java/kr/co/itcen/fa/repository/menu11/Menu50Repository.java
@@ -13,6 +13,7 @@ import kr.co.itcen.fa.util.PaginationUtil;
 import kr.co.itcen.fa.vo.SectionVo;
 import kr.co.itcen.fa.vo.menu01.CustomerVo;
 import kr.co.itcen.fa.vo.menu11.BankVo;
+import kr.co.itcen.fa.vo.menu11.LTermdebtVo;
 import kr.co.itcen.fa.vo.menu11.PdebtVo;
 import kr.co.itcen.fa.vo.menu11.RepayVo;
 /**
@@ -159,6 +160,15 @@ public class Menu50Repository {
 
 	public List<CustomerVo> selectNameFromCustomer(String name) {
 		return sqlSession.selectList("menu50.selectVoucherNo", name);
+	}
+
+	public List<PdebtVo> selectList(Long[] no) {
+		List<Long>listNo = new ArrayList<Long>();
+		for(Long no1: no) {
+			listNo.add(no1);
+		}
+		List<PdebtVo> list = sqlSession.selectList("menu50.selectpdebtlist",listNo);
+		return list;
 	}
 	
 }
