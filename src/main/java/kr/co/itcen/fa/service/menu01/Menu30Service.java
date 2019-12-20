fcada03c1a472537e04512a9e372cdd211622ed5
BIT <BIT@DESKTOP-LRHLNNS>
2019-12-20 13:13:23 +0900

    전월이월 Test

--- src/main/java/kr/co/itcen/fa/service/menu01/Menu30Service.java
+++ src/main/java/kr/co/itcen/fa/service/menu01/Menu30Service.java
@@ -1,5 +1,6 @@
 package kr.co.itcen.fa.service.menu01;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import kr.co.itcen.fa.dto.DataResult;
 import kr.co.itcen.fa.repository.menu01.Menu30Repository;
 import kr.co.itcen.fa.util.PaginationUtil;
+import kr.co.itcen.fa.vo.menu01.PreviousVo;
 import kr.co.itcen.fa.vo.menu01.ReceiptVo;
 import kr.co.itcen.fa.vo.menu17.StatementDataVo;
 
@@ -35,13 +37,89 @@ public class Menu30Service {
 		List<ReceiptVo> list = menu30Repository.list(pagination,revo);
 		dataResult.setDatas(list);
 		
-		//테스트용
-		System.out.println("결과 조회 :" + list.toString());
+		List<StatementDataVo> statementDataList = menu30Repository.statementData();
+		closingEntries(statementDataList);
 		
 		return dataResult;
 	}
 	
-	public void closingEntries(List<StatementDataVo> vo) {
+	public void closingEntries(List<StatementDataVo> sVo) {
+		if(sVo==null) {								//전달값이 없을경우 그냥 반환
+			return;
+		}
+		List<PreviousVo> pVo = new ArrayList<PreviousVo>();
 		
+		for(int i=0; i<sVo.size();i++) {
+			if(sVo.get(i).getAccountNo()>=4000000) {
+				continue;
+			}
+			
+			if(exist(pVo, sVo.get(i))) {
+				if(get(pVo, sVo.get(i)).getAmountFlag().equals(sVo.get(i).getAmountFlag())) {
+					get(pVo, sVo.get(i)).setAmount(get(pVo, sVo.get(i)).getAmount() + sVo.get(i).getAmount());
+				}else {
+					get(pVo, sVo.get(i)).setAmount(get(pVo, sVo.get(i)).getAmount() - sVo.get(i).getAmount());
+				}
+				
+			}else {
+				PreviousVo vo = new PreviousVo();
+				vo.setAccountNo(sVo.get(i).getAccountNo());
+				vo.setCustomerNo(sVo.get(i).getCustomerNo());
+				if(vo.getAccountNo().toString().substring(0,1).equals("1")) {
+					vo.setAmountFlag("d");
+				}else {
+					vo.setAmountFlag("c");
+				}
+				
+				if(vo.getAmountFlag().equals(sVo.get(i).getAmountFlag())) {
+					vo.setAmount(sVo.get(i).getAmount());
+				}else {
+					vo.setAmount(-sVo.get(i).getAmount());
+				}
+				
+				
+				pVo.add(vo);
+			}
+		}
+		for(int i=0 ;i<pVo.size();i++) {
+			System.out.print(pVo.get(i).toString());
+			System.out.println();
+		}
+	}	
+	
+	
+	public boolean exist(List<PreviousVo> pVo, StatementDataVo sVo) {			//존재하는지 여부
+		if(pVo==null) {
+			return false;
+		}
+		for(int i=0; i<pVo.size(); i++) {
+			if(pVo.get(i).exist(sVo.getAccountNo(),sVo.getCustomerNo())) {
+				
+				return true;
+			}
+		}
+		return false;
 	}
+	
+	public PreviousVo get(List<PreviousVo> pVo, StatementDataVo sVo) {			//값얻기
+		for(int i=0; i<pVo.size(); i++) {
+			if(pVo.get(i).exist(sVo.getAccountNo(),sVo.getCustomerNo())) {
+				return pVo.get(i);
+			}
+		}
+		return null;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
