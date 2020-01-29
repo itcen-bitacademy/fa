1e2de848264ad67d85573d01541de4562abc879d
Tae Y. Ahn <forcetyty@gmail.com>
2020-01-29 20:01:03 +0900

    매출관리 : 그래프 : 초기작업

---
+++ src/main/java/kr/co/itcen/fa/controller/menu12/Menu67Controller.java
@@ -0,0 +1,48 @@
+package kr.co.itcen.fa.controller.menu12;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.itcen.fa.security.Auth;
+import kr.co.itcen.fa.service.menu12.Menu67Service;
+
+/**
+ * 
+ * @author 반현길
+ * 단기차입금관리
+ *
+ */
+@Auth
+@Controller
+@RequestMapping("/" + Menu67Controller.MAINMENU)
+public class Menu67Controller {
+	
+	public static final String MAINMENU = "12";
+	public static final String SUBMENU = "67";
+	
+	/*@Autowired
+	private Menu67Service menu67Service;*/
+	
+	//기본
+/*	@RequestMapping({"",  "/add" })
+	public String add(Model model) {
+		return MAINMENU + "/" + SUBMENU + "/add";
+	}
+	
+	@RequestMapping("/statistics")
+	public String statistics() {
+		return MAINMENU + "/" + SUBMENU + "/statistics";
+	}*/
+	
+	
+	@RequestMapping({"/" + SUBMENU, "/" + SUBMENU + "/list" })
+	public String list(Model model) {
+		
+		return MAINMENU + "/" + SUBMENU + "/statistics";
+	}
+	
+	
+	
+}
